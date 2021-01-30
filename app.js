e8a2be0447f526615ca0181cc5694190b62b833e
peterfchris <peter.f.chris@gmail.com>
2021-01-30 13:17:25 -0700

    Get Posts functionality added

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = new easyHTTP 
+
+// Get Posts
+http.get('http://jsonplaceholder.typicode.com/posts', function(err, posts){
+  if(err){
+    console.log(err)
+  } else {
+    console.log(posts)
+  }
+})
